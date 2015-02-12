444b55395b46c6eb7ce5a739b31e8f13c08aef46
Jason Anderson <anderson.jason23@gmail.com>
2015-02-11 23:04:16 -0500

    Added Analyze Muni Page, Other minor fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,7 @@ class PagesController < ApplicationController
   def munitemp
   end
 
+  def invanalyzer
+ 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Munisearch::Application.routes.draw do
   root 'pages#home'
   get 'about' => "pages#about"
   get 'munitemp' => "pages#munitemp"
+  get 'invanalyzer' => "pages#invanalyzer"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
